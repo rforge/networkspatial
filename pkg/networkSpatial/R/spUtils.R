1e73569173f9a93a62c52f1d5b08dd5f5b39321f
zalmquist <zalmquist@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-28 06:38:20 +0000

    S3 issue
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/networkspatial@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/networkSpatial/R/spUtils.R
+++ pkg/networkSpatial/R/spUtils.R
@@ -35,6 +35,14 @@ genpopIndv<-function(spa,total.pop){lapply(spa@data[,total.pop],function(x){cbin
 #################
 ## Register S3
 #S3method(rspop, SpatialPolygonDataFrame)
+rspop <- function(poly, ...){
+	UseMethod("rspop",poly)
+	}
+
+rspop.default <-function(poly,...)
+{
+  stop("Must be a Spatial Polygon")
+}
 
 rspop.SpatialPolygonDataFrame<-function(geo,method = c("uniform", "halton"), stack.rad = 10,stack.dis = 4, household.jitter = 5,longLat=TRUE,household=TRUE,list.pop){
 #require(rgdal)
